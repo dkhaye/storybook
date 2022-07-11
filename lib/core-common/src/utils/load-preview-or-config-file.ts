ba6eb0d2f0522b9896891ee5eca37a41757a77ee
Norbert de Langen <ndelangen@me.com>
2022-07-11 18:42:18 +0200

    remove old code, incorrectly throwing an error

--- lib/core-common/src/utils/load-preview-or-config-file.ts
+++ lib/core-common/src/utils/load-preview-or-config-file.ts
@@ -1,19 +1,10 @@
 import path from 'path';
-import { dedent } from 'ts-dedent';
 import type { Options } from '../types';
 
 import { getInterpretedFile } from './interpret-files';
 
 export function loadPreviewOrConfigFile({ configDir }: Options) {
   const storybookPreviewPath = getInterpretedFile(path.resolve(configDir, 'preview'));
-
-  if (storybookPreviewPath) {
-    throw new Error(dedent`
-      You have both a "config.js" and a "preview.js", remove the "config.js" file from your configDir (${path.resolve(
-        configDir,
-        'config'
-      )})`);
-  }
 
   return storybookPreviewPath;
 }
